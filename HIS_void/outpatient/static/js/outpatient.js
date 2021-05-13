6294d4b549059babf26345f68bdb4d00a53dc084
肖云冲 <444049406@qq.com>
2021-05-13 17:36:25 +0800

    将his中拆分到各个app中（templates、static、views）

---
+++ HIS_void/outpatient/static/js/outpatient.js
@@ -0,0 +1,224 @@
+// 病历首页查询
+function QueryBLSY(p_no) {
+    let URL = '/OutpatientAPI';
+    $.ajax({
+        type: "get",
+        url: URL,
+        dataType: 'json',
+        data: {
+            p_no: p_no,
+            information: 'BLSY'
+        },
+        success: function (data) {
+            document.getElementById("HZZS").setAttribute('placeholder', data.HZZS);
+            document.getElementById("ZLQK").setAttribute('placeholder', data.ZLQK);
+            document.getElementById("JWBS").setAttribute('placeholder', data.JWBS);
+            document.getElementById("GMBS").setAttribute('placeholder', data.GMBS);
+            document.getElementById("TGJC").setAttribute('placeholder', data.TGJC);
+            document.getElementById("FBSJ").setAttribute('placeholder', data.FBSJ);
+            document.getElementById("no").setAttribute('placeholder', data.no);
+            document.getElementById("name").setAttribute('placeholder', data.name);
+            document.getElementById("gender").setAttribute('placeholder', data.gender);
+            document.getElementById("age").setAttribute('placeholder', data.age);
+            document.getElementById("BLSY_a").click();
+        },
+        error: function (err) {
+            alert("请求服务器失败！");
+            console.log(err);
+        },
+    })
+}
+
+// 查询待诊患者
+function QueryDZHZ() {
+    let URL = '/OutpatientAPI';
+    console.log(URL);
+    $.ajax({
+        type: "get",
+        url: URL,
+        dataType: 'json',
+        data: {
+            d_no: '000000',
+            information: 'DZHZ'
+        },
+        success: function (data) {
+            console.log(data);
+            for (let i = 0; i < data.length; i++) {
+                let patient = data[i];
+                let td = '<td>' + patient.name + '</td>';
+                let td1 = '<td>' + patient.status + '</td>';
+                let p_no = patient.p_no
+                console.log(p_no)
+                let tr = $("<tr onclick='QueryBLSY(this.p_no)'></tr>");
+                tr.append(td);
+                tr.append(td1);
+                $("#DZHZ").append(tr);
+            }
+        },
+        error: function (err) {
+            alert("请求服务器失败！");
+            console.log(err);
+        }
+    });
+}
+
+
+// 查询诊中患者
+function QueryZZHZ() {
+    let URL = '/OutpatientAPI';
+    console.log(URL);
+    $.ajax({
+        type: "get",
+        url: URL,
+        dataType: 'json',
+        data: {
+            d_no: '000000',
+            information: 'ZZHZ'
+        },
+        success: function (data) {
+            console.log(data);
+            for (let i = 0; i < data.length; i++) {
+                let patient = data[i];
+                let td = '<td>' + patient.name + '</td>';
+                let td1 = '<td>' + patient.status + '</td>';
+                let p_no = patient.p_no
+                console.log(p_no)
+                let tr = $("<tr onclick='QueryJCJG(this.p_no)'></tr>");
+                tr.append(td);
+                tr.append(td1);
+                $("#ZZHZ").append(tr);
+            }
+        },
+        error: function (err) {
+            alert("请求服务器失败！");
+            console.log(err);
+        }
+    });
+    self.location = '#jian_cha_jian_yan'
+}
+
+
+// 检查结果查询
+function QueryJCJY(p_no) {
+    let URL = '/OutpatientAPI';
+
+    $.ajax({
+        type: "get",
+        url: URL,
+        dataType: 'json',
+        data: {
+            p_no: p_no,
+            information: 'JCJY'
+        },
+        success: function (data) {
+            console.log(data);
+            document.getElementById("HZZS").setAttribute('placeholder', data.HZZS);
+            document.getElementById("ZLQK").setAttribute('placeholder', data.ZLQK);
+            document.getElementById("JWBS").setAttribute('placeholder', data.JWBS);
+            document.getElementById("GMBS").setAttribute('placeholder', data.GMBS);
+            document.getElementById("TGJC").setAttribute('placeholder', data.TGJC);
+            document.getElementById("FBSJ").setAttribute('placeholder', data.FBSJ);
+            document.getElementById("no").setAttribute('placeholder', data.no);
+            document.getElementById("name").setAttribute('placeholder', data.name);
+            document.getElementById("gender").setAttribute('placeholder', data.gender);
+            document.getElementById("age").setAttribute('placeholder', data.age);
+            document.getElementById("JCJY_a").click();
+        },
+        error: function (err) {
+            alert("请求服务器失败！");
+            console.log(err);
+        },
+    })
+}
+
+QueryZZHZ()
+QueryDZHZ()
+
+/*
+$(document).ready(function () {
+    $("#txtSearch").keyup(function () {
+        if ($("#txtSearch").val() == "") {
+            $("#search-result").hide();
+        } else {
+            $.ajax({
+                type: "get",
+                url: "/QueryMZYSView",
+                dataType: 'json',
+                cache: true,
+                data: {
+                    information: 'CFKJ'
+                },
+                success: function (data) {
+                    console.log(data);
+                    var lists = "<ul class='list-group list-group-flush' style='list-style:none;text-align:left;'>";
+                    if (data.length > 0) {
+                        for (i = 0; i < data.length; i++) {
+                            lists += "<li style='padding: 0px 5px' class='list-group-item' onclick='liClick(this)'>" + data[i].name + "</li>";//遍历出每一条返回的数据
+                        }
+                        lists += "</ul>";
+                        $("#search-result").html(lists).show();//将搜索到的结果展示出来
+                    }
+                },
+                error: function (XMLHttpRequest, textStatus, errorThrown) {
+                    // 状态码
+                    alert(XMLHttpRequest.status);
+                }
+            })
+        }
+    });
+    $('#txtSearch').keydown(function () {
+        $("#search-result").hide();
+        $('#search-result').empty();
+
+    })
+    $('#search').blur(function () {
+        $('#search-result').empty();
+    })
+});
+
+function liClick(data) {
+    $("#search-result").hide();
+    $("#txtSearch").val($(data).text());
+}
+*/
+
+// 删除药品
+function deleteMedicine(e) {
+    console.log(e);
+    console.log(e.name);
+    let tag = document.getElementById(e.name);
+//删除
+    tag.parentNode.removeChild(tag);
+    totalPrice();
+}
+
+
+// 添加药品
+function addMedicine(medicine_obj, num) {
+    // 选中药品添加的表格body
+    let medicine_body = $("#medicine_body");
+    tr_html = '<tr id="medicine' + medicine_obj.no + '"><th scope="col">' + medicine_obj.name + '</th>' +
+        '<th scope="col">' + num + '</th>' +
+        '<th scope="col">' + medicine_obj.price + '</th>' +
+        '<th name="total_price" scope="col">' + parseFloat(medicine_obj.price) * parseInt(num) + '</th>' +
+        '<td><span>' +
+        '<a name="medicine' + medicine_obj.no +
+        '" href="javascript:void(0)" onclick="deleteMedicine(this)" data-toggle="tooltip" data-placement="top" title="Close">' +
+        '<i class="fa fa-close color-danger"></i></a></span> </td></tr>';
+
+    medicine_body.append(tr_html);
+    totalPrice();
+}
+
+// 计算总价
+function totalPrice() {
+    let total_prices = document.getElementsByName('total_price');
+    console.log(total_prices);
+    let total = 0;
+    for (let i = 0; i < total_prices.length; i++) {
+        console.log(i);
+        total += parseFloat(total_prices[i].innerText);
+        console.log(total_prices[i]);
+    }
+    $("#medicine_count").text(total);
+}
--- HIS_void/outpatient/views.py
+++ HIS_void/outpatient/views.py
@@ -1,3 +1,121 @@
-from django.shortcuts import render
+from time import sleep
+
+from django.http import JsonResponse
+from django.shortcuts import render, redirect
 
 # Create your views here.
+from django.urls import reverse
+from django.views import View
+
+
+class OutpatientView(View):
+    template_name = 'outpatient-workspace.html'
+
+    def get(self, request):
+        # 检验信息数据
+        JYXX = [
+            {
+                'name': "临床检查",
+                'content': ['临床检查1', '临床检查2', ]
+            },
+            {
+                'name': "生物化学",
+                'content': ['生物化学1', '生物化学2', ]
+            },
+        ]
+        return render(request, OutpatientView.template_name, context={"JYXX": JYXX})
+
+
+class OutpatientAPI(View):
+    def get(self, request):
+        # 获取需要查询的信息类型
+        query_information = request.GET.get('information')
+
+        # 病历首页信息查询
+        if query_information == "BLSY":
+            p_no = request.GET.get('p_no')
+            print(p_no)
+            data = {
+                "no": 114514,
+                "name": "肖云冲",
+                "gender": "男",
+                "age": 18,
+                "HZZS": "患者主诉文本",
+                "ZLQK": "治疗情况文本",
+                "JWBS": "既往病史文本",
+                "GMBS": "过敏病史文本",
+                "TGJC": "体格检查文本",
+                "FBSJ": "发病事件文本",
+            }
+
+        # 待诊患者信息查询
+        elif query_information == "DZHZ":
+            data = [
+                {
+                    "p_no": "183771**",
+                    "name": "李国铭",
+                    "status": "危机",
+                },
+                {
+                    "p_no": "183771--",
+                    "name": "肖云冲",
+                    "status": "普通",
+                },
+                {
+                    "p_no": "183771++",
+                    "name": "朱元琛",
+                    "status": "安全",
+                },
+            ]
+            # 传入医生主键，这样可以有选择的返回病人信息
+            d_no = request.GET.get('d_no')
+            print(d_no)
+
+        # 诊中患者信息查询
+        elif query_information == "ZZHZ":
+            data = [
+                {
+                    "p_no": "183771**",
+                    "name": "李国铭",
+                    "status": "检验完成",
+                },
+                {
+                    "p_no": "183771--",
+                    "name": "肖云冲",
+                    "status": "待检验",
+                },
+                {
+                    "p_no": "183771++",
+                    "name": "朱元琛",
+                    "status": "安全",
+                },
+            ]
+            # 传入医生主键，这样可以有选择的返回病人信息
+            d_no = request.GET.get('d_no')
+            print(d_no)
+
+        # 检查结果信息查询
+        elif query_information == "JCJG":
+            data = [
+
+            ]
+
+        # 药品检索
+        elif query_information == "CFKJ":
+            data = [
+                {'name': "多塞平", 'price': 41, 'number': 100, },
+                {'name': "艾司西酞普兰", 'price': 42, 'number': 100, },
+                {'name': "帕罗西汀", 'price': 43, 'number': 100, },
+                {'name': "氟西汀", 'price': 44, 'number': 100, },
+                {'name': "度洛西汀", 'price': 45, 'number': 100, },
+                {'name': "氟伏沙明", 'price': 46, 'number': 100, },
+            ]
+
+        return JsonResponse(data, safe=False)
+
+    def post(self, request):
+        print("================================")
+        print(request.POST)
+        print("================================")
+        sleep(1)
+        return redirect(reverse("outpatient-workspace"))
